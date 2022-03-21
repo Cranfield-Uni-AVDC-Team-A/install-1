80ceab3da5c4e7e42566467221e1a9485cb5d1b1
Nicholas-H-Wilson <n.houlbrooke@hotmail.com>
2022-03-21 17:23:59 +0000

    [NHW] Bugfix

--- fresh_install.sh
+++ fresh_install.sh
@@ -42,37 +42,12 @@ mkdir -p ~/gdp/src
 cp -R  ~/install-1/src ~/gdp/src
 
 
-DEPENDENCIES=""
-apt-get update
-rosdep update
-TEMP=$(rospack list)
-TEMP=$(grep -E "$AEROSTACK_WORKSPACE|UTR" <<< "$TEMP" | cut -d' ' -f1)
-for p in $(echo $TEMP)
-do
-	echo "Calculating Dependencies for $p"
-	TEMP2+=" $(echo -e $(rospack depends $p  2>/dev/null))"
-done
-
-TEMP=$(tr -s [:space:] \\n <<< $TEMP2 | sort | uniq)
-
-TEMP=$(echo "$TEMP" | sed 's/_/-/g')
-TEMP=$(echo "$TEMP" | sed 's/^/ /')
-TEMP=$(echo "$TEMP" | sed "s/ / ros-$ROS_DISTRO-/g")
-for p in $(echo $TEMP)
-do
-	echo "Checking whether $p is installed or not"
-	apt-cache show $p > /dev/null 2>&1
-	test $? -ne 0 || DEPENDENCIES="$DEPENDENCIES $p"
-done
-
-echo "All dependencies installed succesfully"
-
 echo "-----------------------"
 echo "Building"
 echo "-----------------------"
-cd gdp
+cd ~/gdp
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-echo source ~/.bashrc
+source ~/.bashrc
 catkin build
 echo "Built succesfully"
 echo "-----------------------"
